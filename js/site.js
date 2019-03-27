566afbd06128cfabe2036b7548b1a63a562311fa
Gilles Patrick Ngondo <37463219+gillespatrick@users.noreply.github.com>
2019-03-26 20:52:41 -0400

    My first JavaScript

---
+++ js/site.js
@@ -0,0 +1,22 @@
+console.log("External file : site.js");
+
+
+var city;
+var cityFrom = "";
+var cityName = "New York City";
+var x = 32;
+
+//Display
+
+console.log("cityName and x are : ", cityName, x);
+
+var firstname = 'Judith', lastname = ' Desire';
+console.log("Your firstname and last name is ", firstname, lastname);
+var s = '2019';
+var n = 2001;
+console.log("Age is ", s - n);
+console.log("Age is ", s + n);
+console.log("Age  ", s + parseInt(n));
+var escapequotes = "This is Judi's \"city \"name";
+
+console.log("escapequotes ", escapequotes);
